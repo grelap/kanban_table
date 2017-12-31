aebefc0b41264a1a600bb7b2ea5176fa1da4b944
xenogonx <pablez@wp.pl>
2017-12-31 01:44:08 +0100

    css changes

--- js/Board.js
+++ js/Board.js
@@ -1,28 +1,28 @@
 var board = {
-	name: 'Tablica Kanban',
-	createColumn: function(column) {
-	  this.element.append(column.element);
-	  initSortable();
-	},
-	element: $('#board .column-container')
+  name: 'Tablica Kanban',
+  createColumn: function(column) {
+    this.element.append(column.element);
+    initSortable();
+  },
+  element: $('#board .column-container')
 };
 
 $('.create-column')
     .click(function() {
       var columnName = prompt('Wpisz nazwę kolumny');
       $.ajax({
-    		url: baseUrl + '/column',
-    		method: 'POST',
-    		data: {
-            	name: columnName
-    		},
-    		success: function(response){
-    			var column = new Column(response.id, columnName);
-    			board.createColumn(column);
-          	}
+        url: baseUrl + '/column',
+        method: 'POST',
+        data: {
+              name: columnName
+        },
+        success: function(response){
+          var column = new Column(response.id, columnName);
+          board.createColumn(column);
+            }
       });
 });
-	
+
 function initSortable() {
     $('.card-list').sortable({
       connectWith: '.card-list',
